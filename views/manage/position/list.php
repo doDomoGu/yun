7dd20d8585adabcc70125c33b6ce1cb108988521
doDomoGu <dodomo_gu@sina.cn>
2015-11-12 01:09:36 +0800

    隐藏 编辑功能

--- views/manage/position/list.php
+++ views/manage/position/list.php
@@ -30,7 +30,7 @@
                     <td><?=$l->ord?></td>
                     <td><?=$l->name?> <span class="badge" title="<?=PositionFunc::getFullRoute($l->id)?>">i</span></td>
                     <td><?=PositionFunc::getIsLeaf($l->is_leaf)?></td>
-                    <td><?=BaseHtml::a('编辑',['news-add-and-edit','id'=>$l->id],['class'=>'btn btn-primary btn-xs'])?></td>
+                    <td><?=BaseHtml::a('编辑(暂时不可用)',['','id'=>$l->id],['class'=>'btn btn-primary btn-xs disabled'])?></td>
                 </tr>
             <?php endforeach;?>
             <?php endif;?>
