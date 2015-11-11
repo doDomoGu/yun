410345771c8e30022810aa2703d1bf08fd02c95a
doDomoGu <dodomo_gu@sina.cn>
2015-11-12 02:26:13 +0800

    文件夹/目录 新增

---
+++ controllers/manage/DirAction.php
@@ -0,0 +1,68 @@
+<?php
+namespace app\controllers\manage;
+
+use app\components\PositionFunc;
+use yii\base\Action;
+use app\models\Position;
+
+use Yii;
+
+class PositionAction extends Action{
+    public function run(){
+        if(Yii::$app->request->get('install')){
+            $p = new Position();
+            $p->install();
+            exit;
+        }
+        if(Yii::$app->request->get('handleIsLast')){
+            PositionFunc::handleIsLast();
+            exit;
+        }
+
+
+        $this->controller->view->title = '职位/部门 - 列表';
+
+        $p_id = (int)Yii::$app->request->get('p_id');
+
+        $posList_1 = PositionFunc::getDropDownList(0,true,false,1);
+
+        $posList_2 = [];
+
+        $list = [];
+
+        $curPos = Position::find()->where(['id'=>$p_id,'status'=>1])->one();
+
+        if($curPos){
+            $parents = PositionFunc::getParents($p_id);
+            $posLvl_1 = isset($parents[1])?$parents[1]:null;
+            $posLvl_2 = isset($parents[2]) && $posLvl_1?$parents[2]:null;
+            if($posLvl_1){
+               $posList_2 = PositionFunc::getDropDownList($posLvl_1->id,true,false,1);
+            }
+        }else{
+            $posLvl_1 = null;
+            $posLvl_2 = null;
+        }
+
+
+        if($curPos && $curPos->level==2){
+
+
+            $list = PositionFunc::getListArr($p_id,true,true,false);
+            //$where = ['p_id'=>$p_id];
+        }
+        //$list = Position::find()->where($where)->orderBy('ord desc,id desc')->limit(10)->all();
+
+        $params['list'] = $list;
+        $params['posList_1'] = $posList_1;
+        $params['posList_2'] = $posList_2;
+        $params['posLvl_1'] = $posLvl_1;
+        $params['posLvl_2'] = $posLvl_2;
+
+
+        return $this->controller->render('position/list',$params);
+    }
+
+
+
+}
---
+++ models/Dir.php
@@ -0,0 +1,392 @@
+<?php
+
+namespace app\models;
+use yii;
+
+class Dir extends \yii\db\ActiveRecord
+{
+    public $childrenIds;
+
+    public function rules()
+    {
+        return [
+            ['name', 'required'],
+            [['id, status, p_id, ord, is_leaf, is_last, level, is_class'], 'integer'],
+            ['name', 'string', 'max'=>100],
+            [['describe'],'safe']
+        ];
+    }
+/*CREATE TABLE `dir` (
+`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+`name` varchar(100) NOT NULL COMMENT '目录/项目名称',
+`describe` text NOT NULL,
+`type` tinyint(4) unsigned NOT NULL COMMENT '板块ID',
+`more_cate` tinyint(1) unsigned NOT NULL COMMENT '更多的分类',
+`p_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '父位置id,一级位置为0',
+`level` int(4) unsigned DEFAULT '0' COMMENT '层级',
+`is_leaf` tinyint(1) unsigned DEFAULT '0' COMMENT '0:目录;1:项目;',
+`is_last` tinyint(1) unsigned DEFAULT '0' COMMENT '实际排序当前同级下最后一个',
+`ord` int(4) unsigned DEFAULT '0' COMMENT '排序,倒序从大到小',
+`status` tinyint(1) unsigned DEFAULT '0' COMMENT '0:删除;1:正常',
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8*/
+    public function install() {
+        try {
+            $exist = Position::find()->one();
+            if($exist){
+                throw new yii\base\Exception('Position has installed');
+            }
+
+
+            $arr1 = array(
+                array('n'=>'总经办','cl'=>true,'c'=>array(
+                    array('n'=>'总经理','l'=>true),
+                    array('n'=>'副总经理','l'=>true),
+                    array('n'=>'策划总监','l'=>true),
+                    array('n'=>'总经理助理','l'=>true)
+                )
+                ),
+                array('n'=>'综合管理平台','cl'=>true,'c'=>array(
+                    array('n'=>'财务部','cl'=>true,'c'=>array(
+                        array('n'=>'总监','l'=>true),
+                        array('n'=>'副总监','l'=>true),
+                        array('n'=>'经理','l'=>true),
+                        array('n'=>'主管','l'=>true),
+                        array('n'=>'专员','l'=>true)
+                    )
+                    ),
+                    array('n'=>'行政部','cl'=>true,'c'=>array(
+                        array('n'=>'总监','l'=>true),
+                        array('n'=>'副总监','l'=>true),
+                        array('n'=>'经理','l'=>true),
+                        array('n'=>'主管','l'=>true),
+                        array('n'=>'专员','l'=>true)
+                    )
+                    ),
+                    array('n'=>'人事部','cl'=>true,'c'=>array(
+                        array('n'=>'总监','l'=>true),
+                        array('n'=>'副总监','l'=>true),
+                        array('n'=>'经理','l'=>true),
+                        array('n'=>'主管','l'=>true),
+                        array('n'=>'专员','l'=>true)
+                    )
+                    ),
+                    array('n'=>'后勤部','cl'=>true,'c'=>array(
+                        array('n'=>'经理','l'=>true),
+                        array('n'=>'主管','l'=>true),
+                        array('n'=>'专员','l'=>true)
+                    )
+                    ),
+                )
+                ),
+                array('n'=>'开发拓展部','cl'=>true,'c'=>array(
+                    array('n'=>'总监','l'=>true),
+                    array('n'=>'副总监','l'=>true),
+                    array('n'=>'经理','l'=>true),
+                    array('n'=>'主管','l'=>true),
+                    array('n'=>'专员','l'=>true)
+                )
+                ),
+                array('n'=>'市场策划部','cl'=>true,'c'=>array(
+                    array('n'=>'总监','l'=>true),
+                    array('n'=>'副总监','l'=>true),
+                    array('n'=>'经理','l'=>true),
+                    array('n'=>'主管','l'=>true),
+                    array('n'=>'专员','l'=>true)
+                )
+                ),
+                array('n'=>'销售业务部','cl'=>true,'c'=>array(
+                    array('n'=>'项目案场[A]','c'=>array(
+                        array('n'=>'项目管理','c'=>array(
+                            array('n'=>'总监','l'=>true),
+                            array('n'=>'副总监','l'=>true),
+                            array('n'=>'经理','l'=>true),
+                        )
+                        ),
+                        array('n'=>'案场策划','c'=>array(
+                            array('n'=>'副总监','l'=>true),
+                            array('n'=>'经理','l'=>true),
+                            array('n'=>'主管','l'=>true),
+                            array('n'=>'专员','l'=>true)
+                        )
+                        ),
+                        array('n'=>'案场销售','c'=>array(
+                            array('n'=>'客户主管','l'=>true),
+                        )
+                        ),
+                        array('n'=>'案场客服','c'=>array(
+                            array('n'=>'客户代表','l'=>true),
+                        )
+                        ),
+                        array('n'=>'案场行政','c'=>array(
+                            array('n'=>'主管','l'=>true),
+                            array('n'=>'专员','l'=>true),
+                        )
+                        ),
+                    )
+                    ),
+                    array('n'=>'项目案场[B]','c'=>array(
+                        array('n'=>'项目管理','c'=>array(
+                            array('n'=>'总监','l'=>true),
+                            array('n'=>'副总监','l'=>true),
+                            array('n'=>'经理','l'=>true),
+                        )
+                        ),
+                        array('n'=>'案场策划','c'=>array(
+                            array('n'=>'副总监','l'=>true),
+                            array('n'=>'经理','l'=>true),
+                            array('n'=>'主管','l'=>true),
+                            array('n'=>'专员','l'=>true)
+                        )
+                        ),
+                        array('n'=>'案场销售','c'=>array(
+                            array('n'=>'客户主管','l'=>true),
+                        )
+                        ),
+                        array('n'=>'案场客服','c'=>array(
+                            array('n'=>'客户代表','l'=>true),
+                        )
+                        ),
+                        array('n'=>'案场行政','c'=>array(
+                            array('n'=>'主管','l'=>true),
+                            array('n'=>'专员','l'=>true),
+                        )
+                        ),
+                    )
+                    ),
+                )
+                )
+            );
+            $arr1x = array(
+                array('n'=>'总经办','cl'=>true),
+                array('n'=>'综合管理平台','cl'=>true,'c'=>array(
+                    array('n'=>'财务部','cl'=>true),
+                    array('n'=>'行政部','cl'=>true),
+                    array('n'=>'人事部','cl'=>true),
+                    array('n'=>'后勤部','cl'=>true),
+                )
+                ),
+                array('n'=>'开发拓展部','cl'=>true),
+                array('n'=>'市场策划部','cl'=>true),
+                array('n'=>'销售业务部','cl'=>true),
+            );
+            $arr2 = array(
+                array('n'=>'总经办','cl'=>true,'c'=>array(
+                    array('n'=>'总经理','l'=>true),
+                    array('n'=>'副总经理','l'=>true),
+                    array('n'=>'总经理助理','l'=>true)
+                )
+                ),
+                array('n'=>'综合管理平台','cl'=>true,'c'=>array(
+                    array('n'=>'财务部','cl'=>true,'c'=>array(
+                        array('n'=>'总监','l'=>true),
+                        array('n'=>'副总监','l'=>true),
+                        array('n'=>'经理','l'=>true),
+                        array('n'=>'主管','l'=>true),
+                        array('n'=>'专员','l'=>true)
+                    )
+                    ),
+                    array('n'=>'行政部','cl'=>true,'c'=>array(
+                        array('n'=>'总监','l'=>true),
+                        array('n'=>'副总监','l'=>true),
+                        array('n'=>'经理','l'=>true),
+                        array('n'=>'主管','l'=>true),
+                        array('n'=>'专员','l'=>true)
+                    )
+                    ),
+                    array('n'=>'人事部','cl'=>true,'c'=>array(
+                        array('n'=>'总监','l'=>true),
+                        array('n'=>'副总监','l'=>true),
+                        array('n'=>'经理','l'=>true),
+                        array('n'=>'主管','l'=>true),
+                        array('n'=>'专员','l'=>true)
+                    )
+                    ),
+                    array('n'=>'后勤部','cl'=>true,'c'=>array(
+                        array('n'=>'经理','l'=>true),
+                        array('n'=>'主管','l'=>true),
+                        array('n'=>'专员','l'=>true)
+                    )
+                    ),
+                )
+                ),
+                array('n'=>'开发拓展部','cl'=>true,'c'=>array(
+                    array('n'=>'总监','l'=>true),
+                    array('n'=>'副总监','l'=>true),
+                    array('n'=>'经理','l'=>true),
+                    array('n'=>'主管','l'=>true),
+                    array('n'=>'专员','l'=>true)
+                )
+                ),
+                array('n'=>'AE策划部','cl'=>true,'c'=>array(
+                    array('n'=>'总监','l'=>true),
+                    array('n'=>'副总监','l'=>true),
+                    array('n'=>'经理','l'=>true),
+                    array('n'=>'主管','l'=>true),
+                    array('n'=>'专员','l'=>true)
+                )
+                ),
+                array('n'=>'创意部','cl'=>true,'c'=>array(
+                    array('n'=>'部门管理','c'=>array(
+                        array('n'=>'总监','l'=>true),
+                        array('n'=>'副总监','l'=>true),
+                        array('n'=>'经理','l'=>true),
+                    )
+                    ),
+                    array('n'=>'设计','c'=>array(
+                        array('n'=>'指导','l'=>true),
+                        array('n'=>'师','l'=>true),
+                        array('n'=>'专员','l'=>true),
+                        array('n'=>'助理','l'=>true),
+                    )
+                    ),
+                    array('n'=>'文案','c'=>array(
+                        array('n'=>'指导','l'=>true),
+                        array('n'=>'师','l'=>true),
+                        array('n'=>'专员','l'=>true),
+                        array('n'=>'助理','l'=>true),
+                    )
+                    ),
+                )
+                ),
+                array('n'=>'创作部','cl'=>true,'c'=>array(
+                    array('n'=>'创作1部','c'=>array(
+                        array('n'=>'部门管理','c'=>array(
+                            array('n'=>'总监','l'=>true),
+                            array('n'=>'副总监','l'=>true),
+                            array('n'=>'经理','l'=>true),
+                        )
+                        ),
+                        array('n'=>'设计','c'=>array(
+                            array('n'=>'指导','l'=>true),
+                            array('n'=>'师','l'=>true),
+                            array('n'=>'专员','l'=>true),
+                            array('n'=>'助理','l'=>true),
+                        )
+                        ),
+                        array('n'=>'文案','c'=>array(
+                            array('n'=>'指导','l'=>true),
+                            array('n'=>'师','l'=>true),
+                            array('n'=>'专员','l'=>true),
+                            array('n'=>'助理','l'=>true),
+                        )
+                        ),
+                    )
+                    ),
+                    array('n'=>'创作2部','c'=>array(
+                        array('n'=>'部门管理','c'=>array(
+                            array('n'=>'总监','l'=>true),
+                            array('n'=>'副总监','l'=>true),
+                            array('n'=>'经理','l'=>true),
+                        )
+                        ),
+                        array('n'=>'设计','c'=>array(
+                            array('n'=>'指导','l'=>true),
+                            array('n'=>'师','l'=>true),
+                            array('n'=>'专员','l'=>true),
+                            array('n'=>'助理','l'=>true),
+                        )
+                        ),
+                        array('n'=>'文案','c'=>array(
+                            array('n'=>'指导','l'=>true),
+                            array('n'=>'师','l'=>true),
+                            array('n'=>'专员','l'=>true),
+                            array('n'=>'助理','l'=>true),
+                        )
+                        ),
+                    )
+                    ),
+                )
+                )
+            );
+            $arr2x = array(
+                array('n'=>'总经办','cl'=>true),
+                array('n'=>'综合管理平台','cl'=>true,'c'=>array(
+                    array('n'=>'财务部','cl'=>true),
+                    array('n'=>'行政部','cl'=>true),
+                    array('n'=>'人事部','cl'=>true),
+                    array('n'=>'后勤部','cl'=>true),
+                )
+                ),
+                array('n'=>'开发拓展部','cl'=>true),
+                array('n'=>'AE策划部','cl'=>true),
+                array('n'=>'创意部','cl'=>true),
+                array('n'=>'创作部','cl'=>true)
+            );
+            $arr = array(
+                array('n'=>'管理员','cl'=>true,'l'=>true),
+                array('n'=>'颂唐控股','cl'=>true,'c'=>array(
+                    array('n'=>'总部平台','cl'=>true,'c'=>array(
+                        array('n'=>'发展资源中心','cl'=>true),
+                        array('n'=>'企业形象中心','cl'=>true),
+                        array('n'=>'行政管控中心','cl'=>true),
+                        array('n'=>'财务管控中心','cl'=>true),
+                        array('n'=>'市场研究中心','cl'=>true),
+                    )
+                    ),
+                )
+                ),
+                array('n'=>'颂唐地产研究院','cl'=>true),
+                array('n'=>'华麦建筑','cl'=>true),
+                array('n'=>'颂唐地产','cl'=>true,'c'=>array(
+                    array('n'=>'颂唐地产(总公司)','cl'=>true,'c'=>$arr1x),
+                    array('n'=>'上海颂唐地产','cl'=>true,'c'=>$arr1),
+                    array('n'=>'苏州颂唐地产','cl'=>true,'c'=>$arr1),
+                    array('n'=>'无锡颂唐地产','cl'=>true,'c'=>$arr1),
+                    array('n'=>'南京颂唐地产','cl'=>true,'c'=>$arr1),
+                    array('n'=>'安徽颂唐地产','cl'=>true,'c'=>$arr1),
+                    array('n'=>'苏北颂唐地产','cl'=>true,'c'=>$arr1),
+                )
+                ),
+                array('n'=>'XX房屋','cl'=>true),
+                array('n'=>'XX经纪','cl'=>true),
+                array('n'=>'XX置业','cl'=>true),
+                array('n'=>'日鑫商业','cl'=>true),
+                array('n'=>'颂唐广告','cl'=>true,'c'=>array(
+                    array('n'=>'颂唐广告(总公司)','cl'=>true,'c'=>$arr2x),
+                    array('n'=>'苏州颂唐广告','cl'=>true,'c'=>$arr2),
+                    array('n'=>'南京颂唐广告','cl'=>true,'c'=>$arr2),
+                    array('n'=>'安徽颂唐广告','cl'=>true,'c'=>$arr2),
+                )
+                ),
+                array('n'=>'XX公关'),
+                array('n'=>'XX数码互动'),
+                array('n'=>'XX物业'),
+            );
+            $this->array2value($arr,0,1);
+            return true;
+        }catch (\Exception $e)
+        {
+            echo  'Position install failed<br />';
+            $message = $e->getMessage() . "\n";
+            $errorInfo = $e instanceof \PDOException ? $e->errorInfo : null;
+            echo $message;
+            /*echo '<br/><br/>';
+            var_dump($errorInfo);*/
+
+            //throw new \Exception($message, $errorInfo, (int) $e->getCode(), $e);
+            return false;
+        }
+    }
+
+    public function array2value($arr,$pid,$level){
+
+        $sqlbase = "INSERT IGNORE INTO `position`(`name`,`p_id`,`is_leaf`,`is_class`,`level`,`ord`,`status`)
+                VALUES";
+        $ord = 99;
+        foreach($arr as $a){
+            $is_leaf = isset($a['l']) && $a['l']==1?1:0;
+            $is_class = isset($a['cl']) && $a['cl']==1?1:0;
+
+            $sql =$sqlbase."('".$a['n']."',$pid,$is_leaf,$is_class,$level,$ord,1)";
+            $cmd = Yii::$app->db->createCommand($sql);
+            $cmd->execute();
+
+            //$lastId = Yii::app()->db->lastInsertID;
+            if(isset($a['c'])){
+                $this->array2value($a['c'],Yii::$app->db->lastInsertID,$level+1);
+            }
+            $ord--;
+        }
+    }
+}
