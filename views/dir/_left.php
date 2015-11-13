322143f8c899b884db333a830057adb750cc09df
doDomoGu <dodomo_gu@sina.cn>
2015-11-14 01:23:19 +0800

    目录 样式

--- views/dir/_left.php
+++ views/dir/_left.php
@@ -1,2 +1,5 @@
 <?php \app\assets\AppAsset::addCssFile($this,'css/dir-left.css');?>
-共享目录 左侧边栏
+<div style="border:1px solid #aaa;border-radius:10px;height:300px;padding:10px;">
+    <h3>板块目录</h3>
+    <h4>侧边栏</h4>
+</div>
