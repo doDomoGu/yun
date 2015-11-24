54000b37352e1dbefadb8b3fd829b6526ee90f20
doDomoGu <dodomo_gu@sina.cn>
2015-11-24 08:11:15 +0800

    首页 样式&布局

--- views/site/index.php
+++ views/site/index.php
@@ -8,7 +8,6 @@
 <div class="clearfix"></div>
 <div id="site-index">
     <?php for($i=1;$i<=count($list_dirOne);$i++):?>
-
     <div class="item-one <?=$i==count($list_dirOne)?'item-last':''?>">
         <div class="panel panel-default">
             <div class="panel-heading">
@@ -29,6 +28,45 @@
         </div>
     </div>
     <?php endfor;?>
+    <div class="item-two">
+        <div id="email-login" class="panel panel-default">
+            <div class="panel-heading">颂唐邮箱登陆</div>
+            <div class="panel-body">
+                <div class="form-group">
+                    <!--<label for="email_input">邮箱地址</label>-->
+                    <input type="email" class="form-control" id="email_input" placeholder="输入邮箱地址">
+                </div>
+                <div class="form-group">
+                    <!--<label for="email_pwd_input">密码</label>-->
+                    <input type="password" class="form-control" id="email_pwd_input" placeholder="输入密码">
+                </div>
+                <div class="form-group">
+                    <!--<label for="email_pwd_input">密码</label>-->
+                    <button>登陆</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="item-two">
+        <a href="http://songtang.net" target="_blank">
+            <img src="/images/common/tangxun.jpg">
+        </a>
+    </div>
+    <div class="item-two">
+        <a href="http://songtang.net" target="_blank">
+            <img src="/images/common/tangkan.jpg">
+        </a>
+    </div>
+    <div class="item-two">
+        <a href="http://songtang.net" target="_blank">
+            <img src="/images/common/tangjian.jpg">
+        </a>
+    </div>
+    <div class="item-two item-last">
+        <div class="wx">
+            <img src="/images/common/wx.png">
+        </div>
+    </div>
 </div>
 <div class="clearfix"></div>
 <div id="recruitment_list">
