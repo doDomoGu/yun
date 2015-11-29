5a3a00844acbe5b49f6d0e111f915ca91d26b9fc
doDomoGu <dodomo_gu@sina.cn>
2015-11-29 15:47:37 +0800

    管理按钮

--- views/layouts/navbar2.php
+++ views/layouts/navbar2.php
@@ -16,22 +16,26 @@ app\assets\AppAsset::addCssFile($this,'css/layouts/navbar2.css');
     ]];
 
     $navbarItems = ArrayHelper::merge($navbarItems,\app\components\DirFrontFunc::getNavbar());
-if(!yii::$app->user->isGuest)
+if(!yii::$app->user->isGuest){
+    $item2 = [['label' => '资料', 'url' => '/user']];
+    if($this->context->checkIsAdmin())
+        $item2 = ArrayHelper::merge($item2,[['label' => '管理*', 'url' => '/manage']]);
+    $item2 = ArrayHelper::merge($item2,['<li class="divider"></li>']);
+    $item2 = ArrayHelper::merge($item2,[['label' => '退出', 'url' => '/site/logout']]);
+        /*'<li class="dropdown-header">Dropdown Header</li>',
+        ['label' => 'Level 1 - Dropdown B', 'url' => '#'],*/
+
     $navbarItems = ArrayHelper::merge($navbarItems,
         [
-        [
-            'label' => '<span class="glyphicon glyphicon-user" aria-hidden="true"></span>',
-            'items' => [
-                ['label' => '资料', 'url' => '/user'],
-                '<li class="divider"></li>',
-                ['label' => '退出', 'url' => '/site/logout'],
-                /*'<li class="dropdown-header">Dropdown Header</li>',
-                ['label' => 'Level 1 - Dropdown B', 'url' => '#'],*/
-            ],
-            'linkOptions'=>['style'=>'margin-left:40px;'],
-            'encode'=>false
-        ]]
+            [
+                'label' => '<span class="glyphicon glyphicon-user" aria-hidden="true"></span>',
+                'items' => $item2,
+                'linkOptions'=>['style'=>'margin-left:40px;'],
+                'encode'=>false
+            ]]
     );
+}
+
 ?>
 
 
