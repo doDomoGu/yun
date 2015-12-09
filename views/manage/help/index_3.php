de3200bc420567dbbe3427084f05f340ba7edc97
doDomoGu <dodomo_gu@sina.cn>
2015-12-09 08:48:05 +0800

    管理员 帮助中心 部门1

--- views/manage/help/index_3.php
+++ views/manage/help/index_3.php
@@ -3,12 +3,19 @@
 <div style="padding:10px;">
     <div  class="bg-primary" style="padding:10px 0;">
         <ul >
-            <li>3 首页</li>
-            <li>3.1 第一行区域是公司新闻</li>
-            <li>3.2 第二行区域是目录链接</li>
+            <li>3 部门/职位</li>
+            <li>3.1 下拉选框1：业态选择（颂唐机构、颂唐地产、日鑫商业等)</li>
         </ul>
     </div>
     <p class="text-center">
-        <img src="/images/help/3.jpg" class="img-thumbnail">
+        <img src="/images/manage-help/3.jpg" class="img-thumbnail">
+    </p>
+    <div  class="bg-primary" style="padding:10px 0;">
+        <ul >
+            <li>3.2 下拉选框2：地方公司（上海总部、上海颂唐地产、无锡日鑫商业等)</li>
+        </ul>
+    </div>
+    <p class="text-center">
+        <img src="/images/manage-help/3-2.jpg" class="img-thumbnail">
     </p>
 </div>
