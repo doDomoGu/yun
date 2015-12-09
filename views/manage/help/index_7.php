68ed3ceefd813e67dd2d8652cde9bfc66ecd1e0a
doDomoGu <dodomo_gu@sina.cn>
2015-12-09 10:09:58 +0800

    管理员 帮助中心

--- views/manage/help/index_7.php
+++ views/manage/help/index_7.php
@@ -2,10 +2,9 @@
 <div style="padding:10px;">
     <div class="bg-primary" style="padding:10px 0;">
         <ul >
-            <li>7 消息通知</li>
-            <li>7.1 系统通知
+            <li>
+                7 消息通知
                 <ul>
-                    <li>由管理员发送的通知</li>
                     <li>分为“对单一职员”、“对整个部门/职位”、“对全体职员”，3种发送方式</li>
                 </ul>
             </li>
@@ -13,29 +12,41 @@
         </ul>
     </div>
     <p class="text-center">
-        <img src="/images/help/7.jpg" class="img-thumbnail">
-        <br/>
-        <br/>
-        <img src="/images/help/7-2.jpg" class="img-thumbnail">
+        <img src="/images/manage-help/7.jpg" class="img-thumbnail">
     </p>
-    <div  class="bg-primary" style="padding:10px 0;">
-        <ul >
-            <li>
-                7.2 收件箱（暂无）
-            </li>
-        </ul>
-    </div>
-    <!--<p class="text-center">
-        <img src="/images/help/7-2.jpg" class="img-thumbnail">
-    </p>-->
     <div class="bg-primary" style="padding:10px 0;">
         <ul >
             <li>
-                7.3 发件箱（暂无）
+                7.1 对单一职员发送：需要选择职员ID
             </li>
+
         </ul>
     </div>
-    <!--<p class="text-center">
-        <img src="/images/help/7-3.jpg" class="img-thumbnail">
-    </p>-->
+    <p class="text-center">
+        <img src="/images/manage-help/7-2.jpg" class="img-thumbnail">
+    </p>
+
+    <div class="bg-primary" style="padding:10px 0;">
+        <ul >
+            <li>
+                7.2 对整个部门/职位下归属的职员发送：需要选择部门/职位ID
+            </li>
+
+        </ul>
+    </div>
+    <p class="text-center">
+        <img src="/images/manage-help/7-3.jpg" class="img-thumbnail">
+    </p>
+
+    <div class="bg-primary" style="padding:10px 0;">
+        <ul >
+            <li>
+                7.3 对全体职员发送：无任何选项，对所有职员发送
+            </li>
+
+        </ul>
+    </div>
+    <p class="text-center">
+        <img src="/images/manage-help/7-4.jpg" class="img-thumbnail">
+    </p>
 </div>
