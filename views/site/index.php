9ad27611c4b8cd9ecd6d25841bb92e34f94a89da
doDomoGu <dodomo_gu@sina.cn>
2015-12-13 09:43:27 +0800

    首页下方  唐刊等

--- views/site/index.php
+++ views/site/index.php
@@ -5,6 +5,7 @@
 
 <div class="clearfix"></div>
 <div id="site-index">
+    <section>
     <?php for($i=1;$i<=count($list_dirOne);$i++):?>
     <div class="item-one <?=$i==count($list_dirOne)?'item-last':''?>">
         <div class="panel panel-default">
@@ -13,7 +14,7 @@
                     <?=yii\bootstrap\Html::a($list_dirOne[$i]->name,['/dir','dir_id'=>$list_dirOne[$i]->id])?>
                 </h3>
             </div>
-            <table class="table table-bordered table-hover">
+            <table class="table table-hover">
                 <?php foreach(${'list_'.$i} as $l):?>
                     <tr class="item-count-<?=count(${'list_'.$i})?>">
                         <td class="text-center">
@@ -26,20 +27,39 @@
         </div>
     </div>
     <?php endfor;?>
-    <div class="item-two">
+    </section>
+    <div class="clearfix"></div>
+    <section id="email-login">
+        <article>
+            <span><a href="/" target="_blank">颂唐邮箱登录</a></span>
+        </article>
+        <aside>
+            <span></span>
+        </aside>
+    </section>
+    <div class="clearfix"></div>
+    <section id="article-link">
+        <article id="article-link-1">
+            <span><a href="/" target="_blank">唐 讯</a></span>
+        </article>
+        <article id="article-link-2">
+            <span><a href="/" target="_blank">唐 刊</a></span>
+        </article>
+        <article id="article-link-3">
+            <span><a href="/" target="_blank">唐 鉴</a></span>
+        </article>
+    </section>
+    <!--<div class="item-two">
         <div id="email-login" class="panel panel-default">
             <div class="panel-heading">颂唐邮箱登陆</div>
             <div class="panel-body">
                 <div class="form-group">
-                    <!--<label for="email_input">邮箱地址</label>-->
                     <input type="email" class="form-control" id="email_input" placeholder="输入邮箱地址">
                 </div>
                 <div class="form-group">
-                    <!--<label for="email_pwd_input">密码</label>-->
                     <input type="password" class="form-control" id="email_pwd_input" placeholder="输入密码">
                 </div>
                 <div class="form-group">
-                    <!--<label for="email_pwd_input">密码</label>-->
                     <button>登陆</button>
                 </div>
             </div>
@@ -64,7 +84,7 @@
         <div class="wx">
             <img src="/images/common/wx.png">
         </div>
-    </div>
+    </div>-->
 </div>
 <div class="clearfix"></div>
 <!--<div id="recruitment_list">
