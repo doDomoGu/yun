6a190be1aa11316316b2e9ec9090d66fd9db71d3
doDomoGu <dodomo_gu@sina.cn>
2015-12-14 09:59:38 +0800

    头部 padding-top

--- views/layouts/main_dir.php
+++ views/layouts/main_dir.php
@@ -20,7 +20,7 @@ use app\assets\AppAsset;
 
 <div class="wrap">
     <?php echo $this->render($this->context->navbarView); /* 引入导航栏 */?>
-    <div class="container">
+    <div class="container" style="padding-top:85px;">
         <div id="dir-sidebar">
             <?=$this->render('/dir/_left',['dir_id'=>$this->params['dir_id']])?>
         </div>
