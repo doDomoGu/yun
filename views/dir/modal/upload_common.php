04d4049a08dfb0cc1c1d478d8751d383fd6511a6
doDomoGu <dodomo_gu@sina.cn>
2015-12-21 20:46:43 +0800

    上传 对话框 提示信息

--- views/dir/modal/upload_common.php
+++ views/dir/modal/upload_common.php
@@ -18,7 +18,9 @@ Modal::begin([
             <p>
                 <input type="hidden" id="fileurl" name="fileurl" value="" class="col-lg-6" />
             </p>
-            
+            <div class="alert alert-danger" role="alert">
+                *上传文件较大时，进度条完成前，请勿操作当前页面
+            </div>
             <div class="clearfix" id="fileurl_upload_txt"></div>
 <?php            echo Progress::widget([
             'percent' => 0,
