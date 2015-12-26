277edc4fa92030ac65b4a0f11a49fd21c6583ca2
doDomoGu <dodomo_gu@sina.cn>
2015-12-26 20:44:48 +0800

    帮助信息 2 3 更新

--- views/help/index_3.php
+++ views/help/index_3.php
@@ -8,11 +8,31 @@
     <div  class="bg-primary" style="padding:10px 0;">
         <ul >
             <li>3 首页</li>
-            <li>3.1 第一行区域是公司新闻</li>
-            <li>3.2 第二行区域是目录链接</li>
+            <li>
+                3.1 上部
+                <ul>
+                    <li>1) 全屏滚动轮播公司新闻</li>
+                    <li>2) 左侧固定显示招聘信息</li>
+                </ul>
+            </li>
         </ul>
     </div>
     <p class="text-center">
         <img src="/images/help/3.jpg" class="img-thumbnail">
+    </p>
+    <div  class="bg-primary" style="padding:10px 0;">
+        <ul >
+            <li>3.2 下部
+                <ul>
+                    <li>1) solgan标语</li>
+                    <li>2) 目录文件夹列表页链接</li>
+                    <li>3) 颂唐公司邮箱登陆表单</li>
+                    <li>4) 唐讯、唐刊、唐鉴链接</li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+    <p class="text-center">
+        <img src="/images/help/3-2.jpg" class="img-thumbnail">
     </p>
 </div>
