59ae003dfafaf722e0233a2c2eaa1017b27fd652
doDomoGu <dodomo_gu@sina.cn>
2015-12-29 21:08:48 +0800

    显示目录权限 个人

--- views/user/permission_list.php
+++ views/user/permission_list.php
@@ -3,6 +3,8 @@
     use app\components\PermissionFunc;
     use app\components\DirFunc;
     use app\components\CommonFunc;
+
+    app\assets\AppAsset::addCssFile($this,'css/main/user/permission-list.css')
 ?>
 
 <section>
@@ -17,7 +19,6 @@
             <th>#</th>
             <th>目录</th>
             <th>类型</th>
-            <th width="80" class="text-center">全选</th>
 
             <th width="80" class="text-center"><?=PermissionFunc::getPermissionTypeNameCn2('upload_common')?></th>
             <th width="80" class="text-center"><?=PermissionFunc::getPermissionTypeNameCn2('download_common')?></th>
@@ -44,18 +45,19 @@
                     <td><?=DirFunc::getIsLeaf($l->is_leaf)?></td>
                     <?php if($l->is_leaf):?>
                         <input type="hidden" name="pm[<?=$l->id?>][edit]" />
-                        <td class="text-center"><?=BaseHtml::checkbox('pm['.$l->id.'][all]',isset($pmCheck[$l->id]) && count($pmCheck[$l->id])==4?true:false,['data-pid'=>$l->id,'class'=>'row-check'])?></td>
-                        <td class="text-center real-checkbox"><?=BaseHtml::checkbox('pm['.$l->id.'][11]',isset($pmCheck[$l->id][11])?true:false,['class'=>'pm-'.$l->id,'id'=>'pm-'.$l->id.'-11'])?></td>
-                        <td class="text-center real-checkbox"><?=BaseHtml::checkbox('pm['.$l->id.'][12]',isset($pmCheck[$l->id][12])?true:false,['class'=>'pm-'.$l->id,'id'=>'pm-'.$l->id.'-12'])?></td>
-                        <td class="text-center real-checkbox"><?=BaseHtml::checkbox('pm['.$l->id.'][21]',isset($pmCheck[$l->id][21])?true:false,['class'=>'pm-'.$l->id,'id'=>'pm-'.$l->id.'-21'])?></td>
-                        <td class="text-center real-checkbox"><?=BaseHtml::checkbox('pm['.$l->id.'][32]',isset($pmCheck[$l->id][32])?true:false,['class'=>'pm-'.$l->id,'id'=>'pm-'.$l->id.'-32'])?></td>
+                        <?php $check11 = isset($pmCheck[$l->id][11]);?>
+                        <td class="text-center real-checkbox <?=$check11?'checked':''?>"><?=BaseHtml::checkbox('pm['.$l->id.'][11]',$check11?true:false,['class'=>'pm-'.$l->id,'id'=>'pm-'.$l->id.'-11'])?></td>
+                        <?php $check12 = isset($pmCheck[$l->id][12]);?>
+                        <td class="text-center real-checkbox <?=$check12?'checked':''?>"><?=BaseHtml::checkbox('pm['.$l->id.'][12]',$check12?true:false,['class'=>'pm-'.$l->id,'id'=>'pm-'.$l->id.'-12'])?></td>
+                        <?php $check21 = isset($pmCheck[$l->id][21]);?>
+                        <td class="text-center real-checkbox <?=$check21?'checked':''?>"><?=BaseHtml::checkbox('pm['.$l->id.'][21]',$check21?true:false,['class'=>'pm-'.$l->id,'id'=>'pm-'.$l->id.'-21'])?></td>
+                        <?php $check32 = isset($pmCheck[$l->id][32]);?>
+                        <td class="text-center real-checkbox <?=$check32?'checked':''?>"><?=BaseHtml::checkbox('pm['.$l->id.'][32]',$check32?true:false,['class'=>'pm-'.$l->id,'id'=>'pm-'.$l->id.'-32'])?></td>
                     <?php else:?>
-                        <input type="hidden" id="p-<?=$l->id?>-childs"  value="<?=CommonFunc::array2string($l->childrenIds)?>" />
-                        <td class="text-center"> -- </td>
-                        <td class="text-center"><?=BaseHtml::checkbox('pmCheck['.$l->id.']','',['class'=>'column-check','id'=>'pm-'.$l->id.'-11','data-permission'=>11,'data-pid'=>$l->id])?></td>
-                        <td class="text-center"><?=BaseHtml::checkbox('pmCheck['.$l->id.']','',['class'=>'column-check','id'=>'pm-'.$l->id.'-12','data-permission'=>12,'data-pid'=>$l->id])?></td>
-                        <td class="text-center"><?=BaseHtml::checkbox('pmCheck['.$l->id.']','',['class'=>'column-check','id'=>'pm-'.$l->id.'-21','data-permission'=>21,'data-pid'=>$l->id])?></td>
-                        <td class="text-center"><?=BaseHtml::checkbox('pmCheck['.$l->id.']','',['class'=>'column-check','id'=>'pm-'.$l->id.'-32','data-permission'=>32,'data-pid'=>$l->id])?></td>
+                        <td class="text-center">--</td>
+                        <td class="text-center">--</td>
+                        <td class="text-center">--</td>
+                        <td class="text-center">--</td>
                     <?php endif;?>
                 </tr>
             <?php endforeach;?>
