7480f27a9b77705cd07699aa15ee9d2e9ac4331b
doDomoGu <dodomo_gu@sina.cn>
2016-01-04 00:16:43 +0800

    增加 管理中心  文件列表

--- controllers/manage/file/FileAction.php
+++ controllers/manage/file/FileAction.php
@@ -7,6 +7,8 @@ use Yii;
 
 class FileAction extends Action{
     public function run(){
+        $this->controller->view->title = '文件列表 - 管理中心';
+
         $list = File::find()->all();
         $params['list'] = $list;
         return $this->controller->render('file/index',$params);
