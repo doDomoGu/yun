ac74f63839f3d14f1cbe140bafaf567037bb1245
doDomoGu <dodomo_gu@sina.cn>
2016-01-04 21:37:17 +0800

    管理中心  文件列表 1

--- controllers/manage/file/FileAction.php
+++ controllers/manage/file/FileAction.php
@@ -4,13 +4,30 @@ namespace app\controllers\manage\file;
 use app\models\File;
 use yii\base\Action;
 use Yii;
+use yii\data\Pagination;
 
 class FileAction extends Action{
     public function run(){
         $this->controller->view->title = '文件列表 - 管理中心';
+        $search = [
+            'filename'=>''
+        ];
 
-        $list = File::find()->all();
+
+        $list = File::find();
+
+        $count = $list->count();
+        $pageSize = 20;
+        $pages = new Pagination(['totalCount' =>$count, 'pageSize' => $pageSize,'pageSizeParam'=>false]);
+        $list = $list
+            ->offset($pages->offset)
+            ->limit($pages->limit)
+            ->orderBy('id asc')
+            ->all();
+
         $params['list'] = $list;
+        $params['search'] = $search;
+        $params['pages'] = $pages;
         return $this->controller->render('file/index',$params);
     }
 }
--- views/manage/file/index.php
+++ views/manage/file/index.php
@@ -1,1 +1,51 @@
-sss
+<?php
+use yii\bootstrap\BaseHtml;
+//use app\components\CommonFunc;
+//use app\components\PositionFunc;
+use app\components\DirFunc;
+//app\assets\AppAsset::addJsFile($this,'js/main/manage-user.js');
+?>
+
+<table class="table table-bordered">
+    <thead>
+    <tr>
+        <form id="searchForm" method="post">
+            <th>#</th>
+            <th><input id="s_filename" name="search[filename]" value="<?=$search['filename']?>" size="14" /></th>
+            <th>--</th>
+            <th>--</th>
+            <th>--</th>
+            <th><button type="button" id="searchBtn" >检索</button></th>
+            <input name="_csrf" type="hidden" id="_csrf" value="<?= Yii::$app->request->csrfToken ?>">
+        </form>
+    </tr>
+    </thead>
+    <thead>
+    <tr>
+        <th>#</th>
+        <th>文件名</th>
+        <th>类型</th>
+        <th>上传职员</th>
+        <th>目录路径</th>
+        <th>操作</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php if(!empty($list)):?>
+        <?php foreach($list as $l):?>
+            <tr>
+                <th scope="row"><?=$l->id?></th>
+                <td><?=$l->filename?></td>
+                <td><?=$l->filetype>0?'<span class="label label-info">文件</span>':'<span class="label label-default">文件夹</span>'?></td>
+                <td><?=$l->uid?></td>
+                <td><?=DirFunc::getFileFullRoute($l->dir_id,$l->p_id)?></td>
+                <td><?=BaseHtml::a('编辑',['user-add-and-edit','id'=>$l->id],['class'=>'btn btn-primary btn-xs'])?></td>
+            </tr>
+        <?php endforeach;?>
+    <?php endif;?>
+    </tbody>
+</table>
+
+<div class="clearfix col-md-12 text-center">
+    <?= \yii\widgets\LinkPager::widget(['pagination' => $pages]); ?>
+</div>
