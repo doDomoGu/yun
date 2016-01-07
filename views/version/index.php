2962977f8c8c0a896562edf8d3568ab86d609cdb
doDomoGu <dodomo_gu@sina.cn>
2016-01-07 23:18:04 +0800

    版本历史 页面

--- views/version/index.php
+++ views/version/index.php
@@ -1,13 +1,15 @@
-
-<div style="padding:10px;">
-    <div  class="bg-primary" style="padding:10px 0;">
+<?php
+    app\assets\AppAsset::addCssFile($this,'css/main/version/index.css');
+?>
+<section class="version-item">
+    <article class="bg-primary">
+        <h3>版本 1.0   x年x月x日 发布</h3>
         <ul>
-            <li>版本 1.0   x年x月x日 发布</li>
             <li>功能说明一</li>
             <li>功能说明二</li>
         </ul>
-    </div>
-</div>
+    </article>
+</section>
 
 
 <div style="padding:10px;">
