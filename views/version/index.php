aab8cdcfe923b112f55cd6c1224e4d04a8ecd905
doDomoGu <dodomo_gu@sina.cn>
2016-01-08 11:24:03 +0800

    历史版本信息

--- views/version/index.php
+++ views/version/index.php
@@ -1,23 +1,72 @@
 <?php
     app\assets\AppAsset::addCssFile($this,'css/main/version/index.css');
 ?>
+<!-- ●▪· -->
 <section class="version-item">
-    <article class="bg-primary">
-        <h3>版本 1.0   x年x月x日 发布</h3>
-        <ul>
-            <li>功能说明一</li>
-            <li>功能说明二</li>
-        </ul>
+    <article>
+        <h3>版本 0.2   <span>2015年11月8日 发布</span></h3>
+        <dl>
+            <dt>管理中心</dt>
+            <dd>▪ 管理中心菜单侧边栏</dd>
+            <dd>▪ 招聘信息后台管理</dd>
+            <dd>▪ 职员信息后台管理</dd>
+        </dl>
+
     </article>
+
+    <article>
+        <h3>版本 0.2   <span>2015年11月8日 发布</span></h3>
+        <dl>
+            <dt>管理中心</dt>
+            <dd>▪ 管理中心菜单侧边栏</dd>
+            <dd>▪ 招聘信息后台管理</dd>
+            <dd>▪ 职员信息后台管理</dd>
+        </dl>
+
+    </article>
+
+    <article>
+        <h3>版本 0.2   <span>2015年11月8日 发布</span></h3>
+        <dl>
+            <dt>管理中心</dt>
+            <dd>▪ 管理中心菜单侧边栏</dd>
+            <dd>▪ 招聘信息后台管理</dd>
+            <dd>▪ 职员信息后台管理</dd>
+        </dl>
+
+    </article>
+
+    <article>
+        <h3>版本 0.2   <span>2015年11月8日 发布</span></h3>
+        <dl>
+            <dt>管理中心</dt>
+            <dd>▪ 管理中心菜单侧边栏</dd>
+            <dd>▪ 招聘信息后台管理</dd>
+            <dd>▪ 职员信息后台管理</dd>
+        </dl>
+
+    </article>
+    
+    <article>
+        <h3>版本 0.2   <span>2015年11月8日 发布</span></h3>
+        <dl>
+            <dt>管理中心</dt>
+            <dd>▪ 管理中心菜单侧边栏</dd>
+            <dd>▪ 招聘信息后台管理</dd>
+            <dd>▪ 职员信息后台管理</dd>
+        </dl>
+
+    </article>
+
+    <article>
+        <h3>版本 0.1   <span>2015年10月27日 发布</span></h3>
+        <dl>
+            <dt>新框架移植</dt>
+            <dd>▪ url路由配置</dd>
+            <dd>▪ 个人中心页面</dd>
+            <dd>▪ 管理中心后台</dd>
+        </dl>
+
+    </article>
+
 </section>
-
-
-<div style="padding:10px;">
-    <div  class="bg-primary" style="padding:10px 0;">
-        <ul>
-            <li>版本 2.0   x年x月x日 发布</li>
-            <li>功能说明一</li>
-            <li>功能说明二</li>
-        </ul>
-    </div>
-</div>
