7e3dfac6ab40814a0db3435025ab9c1d174d9d5e
doDomoGu <dodomo_gu@sina.cn>
2016-01-10 03:33:57 +0800

    历史版本信息 9

--- views/version/index.php
+++ views/version/index.php
@@ -3,8 +3,11 @@
 ?>
 <!-- ●▪· -->
 <section class="version-item">
+    <?=$this->render('0.9.1.php')?>
+    <?=$this->render('0.9.0.php')?>
     <?=$this->render('0.8.0.php')?>
     <?=$this->render('0.7.0.php')?>
+    <?=$this->render('0.6.1.php')?>
     <?=$this->render('0.6.0.php')?>
     <?=$this->render('0.5.0.php')?>
     <?=$this->render('0.4.0.php')?>
