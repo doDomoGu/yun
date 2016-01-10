a6149133413ec631e1f586e22c93d2a101a83335
doDomoGu <dodomo_gu@sina.cn>
2016-01-10 09:52:51 +0800

    未登录 菜单图标

--- views/layouts/navbar2.php
+++ views/layouts/navbar2.php
@@ -17,70 +17,72 @@ use yii\helpers\ArrayHelper;
 
     $navbarItems = ArrayHelper::merge($navbarItems,\app\components\DirFrontFunc::getNavbar());
 
-if(!yii::$app->user->isGuest){
-    $item2 = [['label' => '职员资料', 'url' => '/user','options'=>['class'=>'user-item']]];
-    $item2 = ArrayHelper::merge($item2,[['label' => '我的上传', 'url' => '/user/file','options'=>['class'=>'user-item']]]);
-    $item2 = ArrayHelper::merge($item2,[['label' => '我的下载', 'url' => '/user/download','options'=>['class'=>'user-item']]]);
-    $item2 = ArrayHelper::merge($item2,[['label' => '我的权限', 'url' => '/user/permission-list','options'=>['class'=>'user-item']]]);
-    $item2 =  ArrayHelper::merge($item2,[['label' => '消息通知', 'url' => '/message/system','options'=>['class'=>'user-item']]]);
-    if($this->context->checkIsAdmin())
-        $item2 = ArrayHelper::merge($item2,[['label' => '管理中心*', 'url' => '/manage','options'=>['class'=>'user-item']]]);
-    $item2 = ArrayHelper::merge($item2,[['label' => '退出', 'url' => '/site/logout','options'=>['class'=>'user-item']]]);
-}else{
-    $item2 = [['label' => '登录', 'url' => '/site/login','options'=>['class'=>'user-item']]];
+    $iconClass = 'glyphicon glyphicon-user';
+    if(!yii::$app->user->isGuest){
+        $item2 = [['label' => '职员资料', 'url' => '/user','options'=>['class'=>'user-item']]];
+        $item2 = ArrayHelper::merge($item2,[['label' => '我的上传', 'url' => '/user/file','options'=>['class'=>'user-item']]]);
+        $item2 = ArrayHelper::merge($item2,[['label' => '我的下载', 'url' => '/user/download','options'=>['class'=>'user-item']]]);
+        $item2 = ArrayHelper::merge($item2,[['label' => '我的权限', 'url' => '/user/permission-list','options'=>['class'=>'user-item']]]);
+        $item2 =  ArrayHelper::merge($item2,[['label' => '消息通知', 'url' => '/message/system','options'=>['class'=>'user-item']]]);
+        if($this->context->checkIsAdmin())
+            $item2 = ArrayHelper::merge($item2,[['label' => '管理中心*', 'url' => '/manage','options'=>['class'=>'user-item']]]);
+        $item2 = ArrayHelper::merge($item2,[['label' => '退出', 'url' => '/site/logout','options'=>['class'=>'user-item']]]);
+    }else{
+        $iconClass = 'glyphicon glyphicon-menu-hamburger';
+        $item2 = [['label' => '登录', 'url' => '/site/login','options'=>['class'=>'user-item']]];
 
-    /*$navbarItems = ArrayHelper::merge(
-        $navbarItems,
+        /*$navbarItems = ArrayHelper::merge(
+            $navbarItems,
+            [
+                [
+                    'label'=>'<span style="border:1px solid #ccc;padding:4px 10px;">登录</span><span class="active-red"></span>',
+                    'url'=>['/site/login'],
+                    'encode'=>false
+                ]
+            ]
+        );*/
+    }
+
+    $item2 = ArrayHelper::merge($item2,['<li class="divider"></li>']);
+    $item2 = ArrayHelper::merge($item2,[['label' => '帮助中心', 'url' => '/help','options'=>['class'=>'user-item']]]);
+    $item2 = ArrayHelper::merge($item2,[['label' => '版本功能', 'url' => '/version','options'=>['class'=>'user-item']]]);
+
+
+    $route = Yii::$app->controller->route;
+    $routeArr = explode('/',$route);
+    $route1 = isset($routeArr[0]) && $routeArr[0] !=''?$routeArr[0]:'';
+    $userActive = $route1 !='' && in_array($route1,['user','manage','message','help'])?true:false;
+    $navbarItems = ArrayHelper::merge($navbarItems,
         [
             [
-                'label'=>'<span style="border:1px solid #ccc;padding:4px 10px;">登录</span><span class="active-red"></span>',
-                'url'=>['/site/login'],
+                'label' => '<span class="'.$iconClass.'" aria-hidden="true"></span><span class="active-red"></span>',
+                'items' => $item2,
+                'active' => $userActive,
+                'linkOptions'=>['style'=>'margin-left:40px;'],
                 'encode'=>false
             ]
         ]
-    );*/
-}
-
-$item2 = ArrayHelper::merge($item2,['<li class="divider"></li>']);
-$item2 = ArrayHelper::merge($item2,[['label' => '帮助中心', 'url' => '/help','options'=>['class'=>'user-item']]]);
-$item2 = ArrayHelper::merge($item2,[['label' => '版本功能', 'url' => '/version','options'=>['class'=>'user-item']]]);
-
-
-$route = Yii::$app->controller->route;
-$routeArr = explode('/',$route);
-$route1 = isset($routeArr[0]) && $routeArr[0] !=''?$routeArr[0]:'';
-$userActive = $route1 !='' && in_array($route1,['user','manage','message','help'])?true:false;
-$navbarItems = ArrayHelper::merge($navbarItems,
-    [
-        [
-            'label' => '<span class="glyphicon glyphicon-user" aria-hidden="true"></span><span class="active-red"></span>',
-            'items' => $item2,
-            'active' => $userActive,
-            'linkOptions'=>['style'=>'margin-left:40px;'],
-            'encode'=>false
-        ]
-    ]
-);
+    );
 
 ?>
 
 
 <?php
-NavBar::begin([
-    'brandLabel' => BaseHtml::img('/images/logo.png'),
-    'brandUrl' => Yii::$app->homeUrl,
-    'options' => [
-        'class' => 'navbar-default',
-    ],
+    NavBar::begin([
+        'brandLabel' => BaseHtml::img('/images/logo.png'),
+        'brandUrl' => Yii::$app->homeUrl,
+        'options' => [
+            'class' => 'navbar-default',
+        ],
 
-]);
+    ]);
 
-echo Nav::widget([
-    'options' => ['class' => 'navbar-nav pull-right'],
-    'items' => $navbarItems
-]);
+    echo Nav::widget([
+        'options' => ['class' => 'navbar-nav pull-right'],
+        'items' => $navbarItems
+    ]);
 
 
-NavBar::end();
+    NavBar::end();
 ?>
 </div>
