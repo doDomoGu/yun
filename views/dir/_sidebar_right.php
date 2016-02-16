17392c36c85464ddd4c648086811ef4d439444af
doDomoGu <dodomo_gu@sina.cn>
2016-02-17 07:03:20 +0800

    个人中心信息 右侧边栏

--- views/dir/_sidebar_right.php
+++ views/dir/_sidebar_right.php
@@ -11,7 +11,7 @@
     </tr>
     <tr>
         <td>姓名</td>
-        <td><?=$user->name?><?=$user->name?><?=$user->name?><?=$user->name?><?=$user->name?><?=$user->name?><?=$user->name?><?=$user->name?></td>
+        <td><?=$user->name?></td>
     </tr>
     <tr>
         <td>入职时间</td>
@@ -47,53 +47,6 @@
     </tr>
     <tr>
         <td>个人岗位职责</td>
-        <td><?=$position->zhize?>个人岗位职责个人岗位职责个人岗位职责个人岗位职责个人岗位职责个人岗位职责个人岗位职责个人岗位职责个人岗位职责个人岗位职责个人岗位职责个人岗位职责个人岗位职责个人岗位职责个人岗位职责个人岗位职责</td>
+        <td><?=$position->zhize?></td>
     </tr>
 </table>
-<!--<br/>
-
-<div class="right-info">
-    <div class="info-heading">
-        个人信息中心
-    </div>
-    <div class="info-list">
-        <span class="info-list-label">姓名</span>
-        <span class="info-list-txt"><?/*=$user->name*/?></span>
-    </div>
-    <div class="info-list">
-        <span class="info-list-label">入职时间</span>
-        <span class="info-list-txt"><?/*=$user->join_date*/?></span>
-    </div>
-    <div class="info-list">
-        <span class="info-list-label">合同到期时间</span>
-        <span class="info-list-txt"><?/*=$user->contract_date*/?>222222222222222222222222</span>
-    </div>
-    <div class="info-list">
-        <span class="info-list-label">个人所属业务平台</span>
-        <span class="info-list-txt"><?/*=$routeArr[1]*/?></span>
-    </div>
-    <div class="info-list">
-        <span class="info-list-label">个人所属地方公司</span>
-        <span class="info-list-txt"><?/*=$routeArr[2]*/?></span>
-    </div>
-    <div class="info-list">
-        <span class="info-list-label">个人所属部门</span>
-        <span class="info-list-txt"><?/*=$routeArr[3]*/?></span>
-    </div>
-    <div class="info-list">
-        <span class="info-list-label">个人职位</span>
-        <span class="info-list-txt"><?/*=$routeArr[4]*/?></span>
-    </div>
-    <div class="info-list">
-        <span class="info-list-label">个人岗位说明</span>
-        <span class="info-list-txt"><?/*=$position->shuoming*/?></span>
-    </div>
-    <div class="info-list">
-        <span class="info-list-label">个人岗位职权</span>
-        <span class="info-list-txt"><?/*=$position->zhiquan*/?></span>
-    </div>
-    <div class="info-list">
-        <span class="info-list-label">个人岗位职责</span>
-        <span class="info-list-txt"><?/*=$position->zhize*/?></span>
-    </div>
-</div>-->
