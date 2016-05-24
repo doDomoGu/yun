d67ada3f0b71135e8e97e21db6d8f1fb6b1d8eb1
mac-gulingjun <gulingjun@caimiao.com>
2016-05-24 22:30:55 +0800

    职员批量导入 暂存

--- controllers/manage/user/UserImportAction.php
+++ controllers/manage/user/UserImportAction.php
@@ -13,44 +13,57 @@ class UserImportAction extends Action{
     public function run(){
         $this->controller->view->title = '职员列表 - 批量导入';
         if(!empty($_POST)){
-            $filename = $_FILES['file']['tmp_name'];
-            if (empty ($filename)) {
-                echo '请选择要导入的CSV文件！';
-                exit;
-            }
-            $handle = fopen($filename, 'r');
+            $wrong = '';
             $list = [];
-            $data_values = '';
-            $result = CommonFunc::input_csv($handle); //解析csv
-            $len_result = count($result);
-            if($len_result>0){
-                //用户名(邮箱)、姓名、职位、性别、生日、手机、座机、入职日期、合同到期日期
-                for ($i = 1; $i < $len_result; $i++) { //循环获取各字段值
-                    $username = $result[$i][0];
-                    $name = iconv('gb2312', 'utf-8', $result[$i][1]); //中文转码
-                    $position_id = $result[$i][2];
-                    $gender = CommonFunc::getGender2(iconv('gb2312', 'utf-8', $result[$i][3]));
-                    //$data_values .= "('$name','$sex','$age'),";
-                    $list[] = [
-                        'username' => $username,
-                        'name' => $name,
-                        'position_id' => $position_id,
-                        'gender' => $gender
-                    ];
-                }
-            //$data_values = substr($data_values,0,-1); //去掉最后一个逗号
-            fclose($handle); //关闭指针
-            /*var_dump($list);exit;
-            var_dump($data_values);exit;*/
-            /*$query = mysql_query("insert into student (name,sex,age) values $data_values");//批量插入数据表中
-            if($query){
-                echo '导入成功！';
+            $file = $_FILES['file'];
+            if (empty ($file['tmp_name']) || substr($file['name'],-3)!='csv') {
+                $wrong = '请选择正确的CSV文件！';
             }else{
-                echo '导入失败！';
-            }*/
+                $handle = fopen($file['tmp_name'], 'r');
+                $data_values = '';
+                $result = CommonFunc::input_csv($handle); //解析csv
+                $len_result = count($result);
+                if($len_result>0){
+                    //用户名(邮箱)、姓名、职位、性别、生日、手机、座机、入职日期、合同到期日期
+                    for ($i = 1; $i < $len_result; $i++) { //循环获取各字段值
+                        $username = $result[$i][0];
+                        $name = iconv('gb2312', 'utf-8', $result[$i][1]); //中文转码
+                        $position_id = $result[$i][2];
+                        $gender = CommonFunc::getGender2(iconv('gb2312', 'utf-8', $result[$i][3]));
+                        $birthday = date('Y-m-d',strtotime($result[$i][4]));
+                        $mobile = $result[$i][5];
+                        $phone = $result[$i][6];
+                        $join_date = date('Y-m-d',strtotime($result[$i][7]));
+                        $contract_date = date('Y-m-d',strtotime($result[$i][8]));
+                        //$data_values .= "('$name','$sex','$age'),";
+                        $list[] = [
+                            'username' => $username,
+                            'name' => $name,
+                            'position_id' => $position_id,
+                            'gender' => $gender,
+                            'birthday' => $birthday,
+                            'mobile' => $mobile,
+                            'phone' => $phone,
+                            'join_date' => $join_date,
+                            'contract_date' => $contract_date
+                        ];
+                    }
+                    //$data_values = substr($data_values,0,-1); //去掉最后一个逗号
+                    fclose($handle); //关闭指针
+                    /*var_dump($list);exit;
+                    var_dump($data_values);exit;*/
+                    /*$query = mysql_query("insert into student (name,sex,age) values $data_values");//批量插入数据表中
+                    if($query){
+                        echo '导入成功！';
+                    }else{
+                        echo '导入失败！';
+                    }*/
+                }
             }
+
             $params['list'] = $list;
             $params['post'] = true;
+            $params['wrong'] = $wrong;
         }else{
             $params['post'] = false;
         }
--- views/manage/user/import.php
+++ views/manage/user/import.php
@@ -6,42 +6,64 @@
     //app\assets\AppAsset::addJsFile($this,'js/main/manage/user/import.js');
 ?>
 
-<div>
-    <form id="addform" action="" method="post" enctype="multipart/form-data">
-    请选择要导入的CSV文件：<br/>
-        <input type="file" name="file">
-        <input type="submit" class="btn" value="导入CSV">
-        <input name="_csrf" type="hidden" id="_csrf" value="<?= Yii::$app->request->csrfToken ?>">
-    </form>
-</div>
-<?php if($post):?>
-<table class="table table-bordered">
-    <thead>
-    <tr>
-        <th>用户名(邮箱)</th>
-        <th>姓名</th>
-        <th>职位</th>
-        <th>性别</th>
-        <!--<th>联系电话(手机)</th>
-        <th>联系电话(座机)</th>
-        <th>状态</th>
-        <th>操作</th>-->
-    </tr>
-    </thead>
-    <tbody>
-    <?php if(!empty($list)):?>
-        <?php foreach($list as $l):?>
-            <tr>
-                <!--<th scope="row"><?/*=$l->id*/?></th>-->
-                <td><?=$l['username']?></td>
-                <td><?=$l['name']?></td>
-                <td><?=PositionFunc::getFullRoute($l['position_id'])?></td>
-                <td><?=CommonFunc::getGender($l['gender'])?></td>
-            </tr>
-        <?php endforeach;?>
-    <?php endif;?>
-    </tbody>
-</table>
 
-<div class="clearfix text-center">共 <?=count($list)?> 个</div>
+<?php if($post):?>
+    <?php if($wrong==''):?>
+        <p class="clearfix well">
+            <a style="display: block;float:left;" href="/manage/user-import" class="btn btn-primary"><< 返回</a>
+            <span style="display: block;width:600px;text-align:center;font-size:20px;float:left;">  导入信息预览</span>
+            <button style="display: block;float:right;" href="/manage/user-import" class="btn btn-success">确认导入 >></button>
+        </p>
+        <table class="table table-bordered">
+            <thead>
+            <tr>
+                <th>用户名(邮箱)</th>
+                <th width="100">姓名</th>
+                <th>职位</th>
+                <th width="60">性别</th>
+                <th width="100">生日</th>
+                <th width="100">手机</th>
+                <th>座机</th>
+                <th width="100">入职日期</th>
+                <th width="100">合同到期</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php if(!empty($list)):?>
+                <?php foreach($list as $l):?>
+                    <tr>
+                        <!--<th scope="row"><?/*=$l->id*/?></th>-->
+                        <td><?=$l['username']?></td>
+                        <td><?=$l['name']?></td>
+                        <td><?=PositionFunc::getFullRoute($l['position_id'])?></td>
+                        <td><?=CommonFunc::getGender($l['gender'])?></td>
+                        <td><?=$l['birthday']?></td>
+                        <td><?=$l['mobile']?></td>
+                        <td><?=$l['phone']?></td>
+                        <td><?=$l['join_date']?></td>
+                        <td><?=$l['contract_date']?></td>
+                    </tr>
+                <?php endforeach;?>
+            <?php endif;?>
+            </tbody>
+        </table>
+
+        <div class="clearfix text-center">共 <?=count($list)?> 个</div>
+    <?php else:?>
+        <p class="clearfix well">
+            <a style="display: block;float:left;" href="/manage/user-import" class="btn btn-primary"><< 返回</a>
+        </p>
+        <div class="alert alert-danger">
+            <?=$wrong?>
+        </div>
+    <?php endif;?>
+<?php else:?>
+    <div class="well">
+        <form id="addform" action="" method="post" enctype="multipart/form-data">
+            请选择要导入的CSV文件：
+            <input type="file" name="file" style="display: inline;">
+            <input type="submit" class="btn btn-primary" value="导入CSV" style="display: inline;" >
+            <input name="_csrf" type="hidden" id="_csrf" value="<?= Yii::$app->request->csrfToken ?>">
+        </form>
+    </div>
 <?php endif;?>
