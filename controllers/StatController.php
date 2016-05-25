786bdff4f5a870957e89c7b79b0559d02f0b7c86
doDomoGu <dodomo_gu@sina.cn>
2016-05-25 14:18:12 +0800

    增加 页面缓存

--- controllers/StatController.php
+++ controllers/StatController.php
@@ -9,13 +9,42 @@ use app\models\Position;
 
 class StatController extends BaseController
 {
+    public function behaviors()
+    {
+        return [
+            [
+                'class' => 'yii\filters\PageCache',
+                'only' => ['position'],
+                'duration' => 600,
+
+                'dependency' => [
+                    'class' => 'yii\caching\DbDependency',
+                    'sql' => 'SELECT COUNT(*) FROM position',
+                ],
+            ],
+        ];
+    }
+
     public function actionPosition()
     {
 
         $this->view->title = '职位/部门 - 列表';
+        /*$cache = yii::$app->cache;
+        if(Yii::$app->request->get('clearCache')){
+            unset($cache['stat_position']);
+        }
+
+        if(isset($cache['stat_position']) && !empty($cache['stat_position'])){
+
+            $list = $cache['stat_position'];
+        }else{
+            $list = 1;
+            //$list = PositionFunc::getListArr(0,true,true);
+            $cache['stat_position'] = $list;
+        }
+        var_dump($list);exit;*/
 
         $list = PositionFunc::getListArr(0,true,true);
-
         $params['list'] = $list;
         /*$params['posList_1'] = $posList_1;
         $params['posList_2'] = $posList_2;
