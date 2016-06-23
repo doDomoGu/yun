32898be2aac50f34fbb8d01d78d5fe18e5754217
doDomoGu <dodomo_gu@sina.cn>
2016-06-23 21:23:28 +0800

    用户登录 修正用户名之前带空格 无法登录的问题

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -61,6 +61,7 @@ class LoginForm extends Model
     public function getUser()
     {
         if ($this->_user === false) {
+            $this->username = trim($this->username);
             $this->_user = UserIdentity::findByUsername($this->username);
         }
 
