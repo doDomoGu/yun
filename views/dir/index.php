6c3c0d19ce297101fa2e3acdcfb26ceab4f7ae11
mac-gulingjun <gulingjun@caimiao.com>
2016-07-04 13:17:05 +0800

    目录页面 样式调整

--- views/dir/index.php
+++ views/dir/index.php
@@ -8,16 +8,18 @@
     'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
 ]) ?>
 <div class="clearfix"></div>
+<div class="dir-main">
 <?php foreach($list as $l):?>
-<div class="dir-item text-center">
-    <a class="alink" href="/dir?dir_id=<?=$l->id?>">
-        <div class="icon">
-            <img src="/images/fileicon/documents.png">
-        </div>
-        <div class="name">
-            <?=$l->name?>
-        </div>
-    </a>
-</div>
+    <div class="dir-item text-center">
+        <a class="alink" href="/dir?dir_id=<?=$l->id?>">
+            <div class="icon">
+                <img src="/images/fileicon/documents.png">
+            </div>
+            <div class="name">
+                <?=\app\components\CommonFunc::mySubstr($l->name,14)?>
+            </div>
+        </a>
+    </div>
 <?php endforeach;?>
+</div>
 <div class="clearfix"></div>
