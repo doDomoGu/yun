f0e3c7b310624e8431e8caa30b129825fb59aa63
doDomoGu <kaka_71936410@qq.com>
2016-09-26 23:36:01 +0800

    版本功能 新增

--- views/version/index.php
+++ views/version/index.php
@@ -1,20 +1,34 @@
 <?php
     app\assets\AppAsset::addCssFile($this,'css/main/version/index.css');
+
+    $arr = [
+        '0.15.1',
+        '0.15.0',
+        '0.14.0',
+        '0.13.0',
+        '0.12.0',
+        '0.11.0',
+        '0.10.0',
+        '0.9.1',
+        '0.9.0',
+        '0.8.0',
+        '0.7.0',
+        '0.6.1',
+        '0.6.0',
+        '0.5.0',
+        '0.4.0',
+        '0.3.0',
+        '0.2.0',
+        '0.1.0',
+    ];
+
+    
+
 ?>
-<!-- ●▪· -->
+
+
 <section class="version-item">
-    <?=$this->render('0.12.0.php')?>
-    <?=$this->render('0.11.0.php')?>
-    <?=$this->render('0.10.0.php')?>
-    <?=$this->render('0.9.1.php')?>
-    <?=$this->render('0.9.0.php')?>
-    <?=$this->render('0.8.0.php')?>
-    <?=$this->render('0.7.0.php')?>
-    <?=$this->render('0.6.1.php')?>
-    <?=$this->render('0.6.0.php')?>
-    <?=$this->render('0.5.0.php')?>
-    <?=$this->render('0.4.0.php')?>
-    <?=$this->render('0.3.0.php')?>
-    <?=$this->render('0.2.0.php')?>
-    <?=$this->render('0.1.0.php')?>
+    <?php foreach($arr as $a):?>
+        <?=$this->render($a.'.php')?>
+    <?php endforeach;?>
 </section>
